7d77f519a9bc64eac2b9e3f676a2cbb7008eb9fd
Tamoghna <tamoghnadutta896@gmail.com>
2025-01-18 23:31:23 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import e from "express";
+dotenv.config();
 
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
 app.listen(3000, () => {
